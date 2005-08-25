25b2c07457080e19485897bb65dba94f14fada99
yhh <yhh>
2005-08-25 17:49:26 +0000

    move the javascript round

---
+++ docs/geography/google/stage1.js
@@ -0,0 +1,32 @@
+    var map = new GMap(document.getElementById("map"));
+    map.centerAndZoom(new GPoint(-4.218750, 54.724620), 12);
+    map.setMapType( _HYBRID_TYPE )
+    map.addControl(new GLargeMapControl());
+    map.addControl(new GMapTypeControl());
+
+
+function onLoad() {
+    // centre map on location of a click
+    GEvent.addListener(map, "moveend", function() {
+      var center = map.getCenterLatLng();
+      var latLngStr =  "lat=" + center.x + ";long=" + center.y + "";
+      document.getElementById('where').innerHTML = latLngStr;
+      document.getElementById('location').value = latLngStr;
+    });
+
+GEvent.addListener(map, 'click', function(overlay, point) {
+  if (overlay) {
+    map.removeOverlay(overlay);
+  } else if (point) {
+    map.addOverlay(new GMarker(point));
+  }
+});
+
+}
+GEvent.addListener(map, 'click', function() {
+      var center = map.getCenterLatLng();
+      var latLngStr =  "lat=" + center.x + ";long=" + center.y + "";
+      document.getElementById('where').innerHTML = latLngStr;
+      document.getElementById('location').value = latLngStr;
+});
+    //]]>

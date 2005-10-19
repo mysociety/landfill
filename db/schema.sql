877f3f923332b83ae02b45fbfa8c7c0287aec3cc
chris <chris>
2005-10-19 14:09:10 +0000

    Add additional useful stuff to the schema.

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,18 @@
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: schema.sql,v 1.4 2005-10-18 15:04:43 chris Exp $
+-- $Id: schema.sql,v 1.5 2005-10-19 14:09:10 chris Exp $
 --
+
+-- convenience functions
+create function epoch(timestamp) returns double precision as
+    'select case when $1 is not null then extract(epoch from $1) else null end'
+    language 'sql';
+    
+create function epoch(timestamptz) returns double precision as
+    'select case when $1 is not null then extract(epoch from $1) else null end'
+    language 'sql';
+
 
 create table secret (
     id integer primary key default(0) check (id = 0),
@@ -34,6 +44,11 @@ create table location (
     lon double precision not null
 );
 
+create index location_lat_idx on location(lat);
+create index location_lon_idx on location(lon);
+
+-- XXX add near query facility from PB
+
 create table item (
     id serial not null primary key,
     -- identity of donor
@@ -43,9 +58,12 @@ create table item (
     category_id integer references category(id),
     description text not null,
     location_id integer not null references location(id),
+    -- about the donor
+--    ipaddr text not null,
+--    referrer text,
     -- state
-    confirmed boolean default (false),
-    available boolean default (true)
+    whenadded timestamp not null default (current_timestamp),
+    whenconfirmed timestamp
 );
 
 create index item_email_idx on item(email);
@@ -55,18 +73,34 @@ create table acceptor (
     email text not null,
     name text not null,
     organisation text not null,
+    charity_number text,
     location_id integer not null references location(id),
     bouncing boolean default (false)
 );
 
 create table acceptor_category_interest (
     acceptor_id integer not null references acceptor(id),
-    category_id integer not null references category(id)
+    category_id integer not null references category(id),
+    primary key(acceptor_id, category_id)
 );
+
+create index acceptor_category_interest_acceptor_id_idx
+    on acceptor_category_interest(acceptor_id);
+    
+create index acceptor_category_interest_category_id_idx
+    on acceptor_category_interest(category_id);
 
 create table acceptor_item_interest (
     acceptor_id integer not null,
     item_id integer not null references item(id),
     whensent timestamp not null default current_timestamp,
-    whenaccepted timestamp
+    whenaccepted timestamp,
+    primary key(acceptor_id, item_id)
 );
+
+create index acceptor_item_interest_acceptor_id_idx
+    on acceptor_item_interest(acceptor_id);
+    
+create index acceptor_item_interest_item_id_idx
+    on acceptor_item_interest(item_id);
+

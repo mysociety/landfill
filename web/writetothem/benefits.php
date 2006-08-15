e5ae71a6035ced20b0ac7d9ee76f8b0f0abf235c
francis <francis>
2006-08-15 09:54:41 +0000

    Draft (not linked to anywhere yet) of more specific benefits of buying WriteToThem for an NGO.

---
+++ web/writetothem/benefits.php
@@ -0,0 +1,50 @@
+<? $title = 'WriteToThem - Benefits'; include_once('../header.php'); ?>
+
+<h1>WriteToThem - Benefits</h1>
+
+<p id="sub">The world&#8217;s most effective way of putting citizens in touch with their representatives</p>
+
+<h2>Your logo and colours</h2>
+
+<p>We customise WriteToThem to have your organisation's branding. This can be 
+anything from a simple coat of paint with your logo in the corner, to a
+complete design looking seemlessly like part of your website.
+</p>
+
+<h2>Letter-writing tips</h2>
+
+<p>On your cobranded site, you can display text reminding your supporters
+of points they might want to make in their letter. This appears just
+above their letter while they are writing it. You can change this text
+according to the campaign you are running.
+</p>
+
+<h2>Return to your site afterwards</h2>
+
+<p>When your supporter confirms their message, they are redirected to a
+confirmation page on your site. This gives you the chance to suggest
+follow up actions. It also lets you track how many people completed
+their message.
+</p>
+
+<h2>Campaign coding</h2>
+
+<p>Have multiple campaigns and keep track of them. You can invent as many
+"campaign codes" as you like. For each one, change both the letter writing
+tips, and the address on the site your support returns to after writing
+their letter. 
+</p>
+
+<h2>Tracking success</h2>
+
+<p>We generate a report for you of how many messages are sent via your
+cobranded site each day. This includes breakdown of type of representative
+- whether MP, MEP, Councillor etc. For all messages sent through WriteToThem
+we monitor whether the constituent got a response within 2 to 3 weeks. We
+also provide this information for your campaign.
+
+<p>
+
+<h2>
+
+<? include_once('../footer.php'); ?>

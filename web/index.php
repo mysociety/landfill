19563412e98946c3548fd2581022d49d5ae39bb7
matthew <matthew>
2006-09-24 20:31:45 +0000

    Show extra paragraph for people who've visited from http://www.dft.gov.uk/stellent/groups/dft_science/documents/page/dft_science_611659.hcsp

--- web/index.php
+++ web/index.php
@@ -1,7 +1,14 @@
-<? include_once('header.php'); ?>
+<? include_once('header.php');
+
+if (isset($_SERVER['HTTP_REFERER']) && preg_match('#www\.dft\.gov\.uk#', $_SERVER['HTTP_REFERER'])) { ?>
+<p>If you're looking for our 
+<a href="http://www.mysociety.org/2006/travel-time-maps/">travel time maps</a>,
+please visit <a href="http://www.mysociety.org/2006/travel-time-maps/">http://www.mysociety.org/2006/travel-time-maps/</a>.</p>
+<? } ?>
 
 <p>Welcome to mySociety.co.uk &mdash; the new not-for-profit trading arm of
-charitable project mySociety.org. mySociety.co.uk sells licenced web service
+charitable project <a href="http://www.mysociety.org/">mySociety.org</a>.
+mySociety.co.uk sells licenced web service
 products based on our award-winning civic websites, particularly focusing on
 the local government and charitable sectors.</p>
 

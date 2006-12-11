012387ae46c4d8d06712c238e665c4cdd4589f6d
francis <francis>
2006-12-11 16:47:12 +0000

    Remove old copy of cvstract schema from its old place, correct comment in moved file

--- db/cvstrac-schema-delta1.sql
+++ db/cvstrac-schema-delta1.sql
@@ -1,11 +1,11 @@
 --
--- cvstrac-schema-changes.sql:
+-- cvstrac-schema-delta1.sql:
 -- Additions to the cvstrac database for our volunteer tasks system.
 --
 -- Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 -- Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: cvstrac-schema-delta1.sql,v 1.1 2006-12-11 15:50:21 ben Exp $
+-- $Id: cvstrac-schema-delta1.sql,v 1.2 2006-12-11 16:47:13 francis Exp $
 --
 
 create table volunteer_interest (

203490cc4324fe493957dfd7562f3cc706cd3fb7
matthew <matthew>
2007-03-13 11:45:14 +0000

    Add HFYC competition.

---
+++ web/councillor-competition/index.php
@@ -0,0 +1,39 @@
+<? $title = 'HearFromYourCouncillor competition'; include_once('../header.php'); ?>
+
+<h1>HearFromYourCouncillor competition</h1>
+
+<p id="sub">Enter our competition and get HearFromYourCouncillor for free.</p>
+
+<p><a href="http://www.hearfromyourmp.com/">HearFromYourMP.com</a>
+is a simple and popular democratic website that
+builds up email lists which MPs can then email to kick-off discussions
+about local issues.</p>
+
+<p>Although only 18 months old HearFromYourMP has already been used by
+over 100 members of Parliament to talk with groups of their
+constituents, and mySociety is now set on transferring this success
+into the council world.</p>
+
+<p>If you've never used HearFromYourMP.com we strongly suggest you sign
+up, and take a quick read through <a
+href="http://www.hearfromyourmp.com/about">our About page</a> explaining
+how it works and what makes it so much more well used than standard
+discussion forums or online consultations. Over 32,000 people are
+signed up across the UK, with the number rising steadily.</p>
+
+<p>The competition is simple. Before April 15th we ask councils who are
+interested simply to tell us what they'd do to ensure
+HearFromYourCouncillor gets a healthy start in their area. The sort of
+thing we're looking for is a commitment to publicise the service from
+the site homepage, or to include it in print materials. After April
+15th we'll assess the different non-monetary offers we've been made,
+and pick two winners based on how seriously we think councils will
+take our offer of a free service. We will then build and customise
+HearFromYourCouncillor in conjunction with local authority web teams,
+and launch as soon as possible after the local elections, running for
+a year for free.</p>
+
+<p>To contact mySociety with any questions about this or any other
+project, please email <a href="mailto:team&#64;mysociety.org">team&#64;mysociety.org</a>.</p>
+
+<? include_once('../footer.php'); ?>

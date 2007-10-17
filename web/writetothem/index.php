28936c443f36475ca4ffffd908efbdb8560e3e86
francis <francis>
2007-10-17 15:03:24 +0000

    Merge separate benefits page.

--- web/writetothem/index.php
+++ web/writetothem/index.php
@@ -8,7 +8,8 @@
 
 <img class="right" align="right" src="writetothem.png" alt="" width="333" height="241">
 <ul>
-<li>Proven track record of attracting high numbers of people who.ve never contacted their representatives before for any reason.
+<li>Proven track record of attracting high numbers of people who've never
+contacted their representatives before for any reason.
 <li>Identify representatives using nothing more than a postcode.
 <li>Users are given the chance to contact any of their representatives, whether councillors, MPs, MEPs, MSPs or other assembly members.
 <li>Helps to direct correspondence to the right tier of government, lowering amount of correspondence sent wrongly to councils.
@@ -26,5 +27,46 @@ anything in between.</p>
 <p>Already linked to by dozens of local authorities around the UK mySociety is
 now starting to offer branded, integrated version (like the one above) to run
 within local authority websites.</p>
+
+<h2>What do you get?</h2>
+
+<ul>
+
+<li><strong>Your logo and colours.</strong>
+
+We customise WriteToThem to have your organisation's branding. This can be 
+anything from a simple coat of paint with your logo in the corner, to a
+complete design looking seemlessly like part of your website.
+
+<li><strong>Letter-writing tips.</strong>
+
+On your cobranded site, you can display text reminding your supporters
+of points they might want to make in their letter. This appears just
+above their letter while they are writing it. You can change this text
+according to the campaign you are running.
+
+<li><strong>Return to your site afterwards.</strong>
+
+When your supporter confirms their message, they are redirected to a
+confirmation page on your site. This gives you the chance to suggest
+follow up actions. It also lets you track how many people completed
+their message.
+
+<li><strong>Campaign coding.</strong>
+
+Have multiple campaigns and keep track of them. You can invent as many
+"campaign codes" as you like. For each one, change both the letter writing
+tips, and the address on the site your support returns to after writing
+their letter. 
+
+<li><strong>Tracking success.</strong>
+
+We generate a report for you of how many messages are sent via your
+cobranded site each day. This includes breakdown of type of representative
+- whether MP, MEP, Councillor etc. For all messages sent through WriteToThem
+we monitor whether the constituent got a response within 2 to 3 weeks. We
+also provide this information for your campaign.
+
+</ul>
 
 <? include_once('../footer.php'); ?>
